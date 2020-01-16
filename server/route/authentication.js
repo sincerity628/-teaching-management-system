0d9cb48bcdfa1c4943a904e780000b8fab85cd91
sincerity628 <14785481094@163.com>
2020-01-17 00:44:09 +0800

    changed the signin api.

--- server/route/authentication.js
+++ server/route/authentication.js
@@ -4,64 +4,55 @@ const connection = require('../database/connection');
 
 // signin
 router.post('/', (req, res) => {
-  let found = false;
+
   const query1 = `select * from student where studentId = ${req.body.num}`;
   connection.query(query1, (error, results) => {
     if(error) {
       return res.status(400).json(error);
     } else {
       if(results.length) {
-        let user = {
-          studentId: results[0].studentId,
-          name: results[0].name,
-          sex: results[0].sex,
-          age: results[0].age,
-          department: results[0].department,
-          role: 'student'
-        };
-        found = true;
-        if(results[0].password !== req.body.password) {
+        let user = results[0];
+        user.role = 'student';
+
+        if(user.password !== req.body.password) {
           return res.status(400).json({
             msg: '密码错误'
           });
         }
+
+        delete user.password;
         return res.json(user);
+      } else {
+        const query2 = `select * from staff where workId = ${req.body.num}`;
+        connection.query(query2, (error, results) => {
+          if(error) {
+            return res.status(400).json(error);
+          } else {
+            if(results.length) {
+              let user = results[0];
+              user.role = 'staff';
+
+              if(user.password !== req.body.password) {
+                return res.status(400).json({
+                  msg: '密码错误'
+                });
+              }
+
+              delete user.password;
+              return res.json(user);
+            } else {
+              // user not found
+              return res.status(400).json({
+                msg: '请检查学号或工号的正确性'
+              });
+            }
+          }
+        });
+
       }
     }
   });
-
-  const query2 = `select * from staff where workId = ${req.body.num}`;
-  connection.query(query2, (error, results) => {
-    if(error) {
-      return res.status(400).json(error);
-    } else {
-      if(results.length) {
-        let user = {
-          studentId: results[0].studentId,
-          name: results[0].name,
-          sex: results[0].sex,
-          age: results[0].age,
-          department: results[0].department,
-          role: 'student'
-        };
-        found = true;
-        if(results[0].password !== req.body.password) {
-          return res.status(400).json({
-            msg: '密码错误'
-          });
-        }
-        return res.json(user);
-      }
-    }
-  });
-
-  if(!found) {
-    return res.status(400).json({
-      msg: '请检查学号或工号的正确性'
-    });
-  }
 
 });
-
 
 module.exports = router;
